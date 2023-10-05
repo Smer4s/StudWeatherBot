d41a537c4d57f5f57cd978fb98c96d50713bc0f8
User <User@DESKTOP-5IPJB2T>
2023-10-05 18:12:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StudWeatherBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
